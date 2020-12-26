193794c7ec15725426890c7660f0a4de73ba3907
jakartacyberteam <76602364+jakartacyberteam@users.noreply.github.com>
2020-12-26 08:17:19 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk"
+echo
+pip install requests
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk:V"
+echo
+pip install mechanize
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk:V"
+echo
+pip install requests bs4
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk:V"
+echo
+pkg install figlet
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk:V"
+echo
+pip2 install requests
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk:V"
+echo
+pip2 install tqdm
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3x Biar Work Wkwkwk:V"
+echo
+pip2 install mechanize
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3X Biar Work Wkwkwk:V"
+echo
+pkg install ruby -y
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan InYour./CYBER Ganteng 3X Biar Work Wkwkwk:V"
+echo
+gem install lolcat
+clear
+figlet Mastah Illusion | lolcat
+sleep 2
+clear
+figlet Mastah Illusion | lolcat
+sleep 2
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+echo
+echo "Langsung Salin Bro python2 crackfb.py" | lolcat
